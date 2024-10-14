0f798cc539a440ce69e0b08662b578c79e820111
Raimon T Simon <raimonrts@gmail.com>
2024-10-14 14:47:57 +0530

    added stepper card

---
+++ components/how-it-works.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Sparkles, UploadCloud, UserCheck } from "lucide-react";
+import React from "react";
+import StepperCard from "./stepperCard";
+
+
+
+function HowItWorks() {
+  return (
+    <div className="flex flex-col gap-16">
+      <div className="max-w-screen-xl flex flex-col gap-2 items-center">
+        <h1 className="text-4xl font-bold text-purple-600">
+          Nursera connects you with top-tier opportunities,
+        </h1>
+        <h1 className="text-4xl font-bold">
+          opening doors you didn’t even know existed.
+        </h1>
+      </div>
+      <div>
+        <div className="grid grid-cols-[1fr_max-content_1fr_max-content_1fr] gap-8">
+          <StepperCard
+            heading="Upload your resume"
+            description=" Add your resume, choose your ideal job title, and get
+                interview-ready.This helps us match you with the right
+                opportunities!"
+            icon={UploadCloud}
+            color="blue"
+          />
+          <div className="w-1"></div>
+          <StepperCard
+            heading="Take A Brief Interview"
+            description=" Spend 10 minutes in a quick interview based on your job title and resume, which will be used to assess your suitability for the job."
+            icon={Sparkles}
+            color="orange"
+          />
+          <div className="w-1"></div>
+          <StepperCard
+            heading="Sit back and relax while matches come to you!"
+            description=" Set your preferences and watch as the AI matches you with
+                suitable job opportunities!"
+            icon={UserCheck}
+            color="green"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HowItWorks;
---
+++ components/stepperCard.tsx
@@ -0,0 +1,40 @@
+import { FunctionComponent, SVGProps } from "react";
+
+type IconType = FunctionComponent<SVGProps<SVGSVGElement>>;
+
+interface StepperCardProps {
+  heading: string;
+  description: string;
+  color: "blue" | "orange" | "green";
+  icon: IconType;
+}
+
+const StepperCard: React.FC<StepperCardProps> = ({
+  heading,
+  description,
+  color,
+  icon: Icon,
+}) => {
+  const backgroundColorClass =
+    color === "blue"
+      ? "bg-blue-500"
+      : color === "orange"
+      ? "bg-orange-500"
+      : "bg-green-500";
+
+  return (
+    <div className="flex flex-col gap-4 items-center">
+      <div
+        className={`w-24 h-24 flex items-center justify-center rounded-full ${backgroundColorClass}`}
+      >
+        <Icon className="w-12 h-12 text-white"  strokeWidth={1.2}/>
+      </div>
+      <div className="flex flex-col gap-2 text-center">
+        <div className="text-lg font-medium">{heading}</div>
+        <div className="text-md text-muted-foreground">{description}</div>
+      </div>
+    </div>
+  );
+};
+
+export default StepperCard;
