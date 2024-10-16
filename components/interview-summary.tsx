2d6daad9428fee2103b218825c4cba4d1e433adc
Raimon T Simon <raimonrts@gmail.com>
2024-10-16 18:07:24 +0530

    interview summary fixes

--- components/interview-summary.tsx
+++ components/interview-summary.tsx
@@ -1,14 +1,14 @@
-"use client";
+'use client';
 
-import { Calendar, Clock, FileText } from "lucide-react";
-import { useRouter } from "next/navigation";
-import { type InterviewData } from "src/types/types";
+import { Calendar, Clock, FileText } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { type InterviewData } from 'src/types/types';
 
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardFooter } from '@/components/ui/card';
 
-import Footer from "./footer";
-import NursanaLogo from "./nursana-logo";
+import Footer from './footer';
+import NursanaLogo from './nursana-logo';
 
 interface InterviewSummaryProps {
   interviewId: string;
@@ -20,67 +20,66 @@ export default function InterviewSummary({
 }: InterviewSummaryProps) {
   const router = useRouter();
   return (
-    <div className=" flex flex-col items-center">
-      <div className="h-[calc(100vh-72px)] pt-10 flex flex-col items-center">
-        <div className="">
-      <NursanaLogo/>
-      </div>
-      <div className="text-2xl font-medium text-center mb-1 flex">
-        <span>Interview completed successfully 🎉</span>
-      </div>
-      <p className="text-center text-muted-foreground mb-6 max-w-xl">
-        Interview successfully completed. Head to the dashboard for more
-        details.
-      </p>
-      <Card className="w-full bg-secondary">
-        <CardContent className="p-4">
-          <h2 className="text-lf  font-medium mb-2">{interviewData.name}</h2>
-          <div className="flex items-center gap-4">
-            <div className="flex items-center text-gray-600">
-              <Clock className="w-5 h-5 mr-2" />
-              <span>{interviewData.candidate_estimated_time}</span>
+    <div className='flex flex-col items-center'>
+      <div className='flex h-[calc(100vh-72px)] flex-col items-center pt-10'>
+        <div className=''>
+          <NursanaLogo />
+        </div>
+        <div className='mb-1 flex text-center text-2xl font-medium'>
+          <span>Interview completed successfully 🎉</span>
+        </div>
+        <p className='mb-6 max-w-2xl text-center text-muted-foreground'>
+          Review all the important details and outcomes from your interview on
+          the dashboard. Get insights, resume feedback, and next steps to help
+          you move forward confidently.
+        </p>
+        <Card className='w-full bg-secondary'>
+          <CardContent className='p-4'>
+            <h2 className='text-lf mb-2 font-medium'>{interviewData.name}</h2>
+            <div className='flex items-center gap-4'>
+              <div className='flex items-center text-gray-600'>
+                <Clock className='mr-2 h-5 w-5' />
+                <span>{interviewData.candidate_estimated_time}</span>
+              </div>
+              <div className='flex items-center text-gray-600'>
+                <Calendar className='mr-2 h-5 w-5' />
+                <span>{new Date(Date.now()).toLocaleDateString()}</span>
+              </div>
             </div>
-            <div className="flex items-center text-gray-600">
-              <Calendar className="w-5 h-5 mr-2" />
-              <span>{new Date(Date.now()).toLocaleDateString()}</span>
+          </CardContent>
+          <CardFooter className='hidden items-center space-x-4 border-t pt-4'>
+            <div className='flex items-center space-x-2 text-gray-600'>
+              <FileText className='h-5 w-5' />
+              <div>
+                <p className='text-sm font-medium'>
+                  {interviewData.candidate_intro_video_url}
+                </p>
+                <p className='text-xs'>
+                  {interviewData.candidate_intro_video_cover_image_url}
+                </p>
+              </div>
             </div>
-            
-          </div>
-
-        </CardContent>
-        <CardFooter className="hidden  items-center space-x-4 border-t pt-4">
-          <div className="flex items-center space-x-2 text-gray-600">
-            <FileText className="w-5 h-5" />
-            <div>
-              <p className="text-sm font-medium">
-                {interviewData.candidate_intro_video_url}
-              </p>
-              <p className="text-xs">
-                {interviewData.candidate_intro_video_cover_image_url}
-              </p>
-            </div>
-          </div>
+            <Button
+              variant='link'
+              className='ml-auto text-blue-600 hover:text-blue-800'
+            >
+              view resume
+            </Button>
+          </CardFooter>
+        </Card>
+        <div className='mt-8 text-center'>
           <Button
-            variant="link"
-            className="ml-auto text-blue-600 hover:text-blue-800"
+            className='w-full'
+            size='lg'
+            onClick={() => {
+              router.push(`/magic-link`);
+            }}
           >
-            view resume
+           View My Dashboard
           </Button>
-        </CardFooter>
-      </Card>
-      <div className="mt-8 text-center">
-        <Button
-          className="w-full"
-          size="lg"
-          onClick={() => {
-            router.push(`/magic-link`);
-          }}
-        >
-          View Resume Feedback
-        </Button>
+        </div>
       </div>
-      </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
