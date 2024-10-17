60988085197ab9132dba173078c009bb551ad4d5
crc <chinmai@aglinthq.com>
2024-10-17 15:58:45 +0530

    fix

--- app/(auth-pages)/auth/confirm/route.ts
+++ app/(auth-pages)/auth/confirm/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server';
 
 import { createClient } from '@/utils/supabase/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: Request) {
   // The `/auth/callback` route is required for the server-side auth flow implemented
   // by the SSR package. It exchanges an auth code for the user's session.
