da04fbfbf5b17b8f4b109c7fc1b3b37e20eb107a
Raimon T Simon <raimonrts@gmail.com>
2024-10-17 18:11:40 +0530

    lint

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,29 +1,27 @@
+import Link from 'next/link';
+import { Footer } from 'react-day-picker';
 
 import NursanaLogo from '@/components/nursana-logo';
 import Section from '@/components/section';
 import { Button } from '@/components/ui/button';
-import Link from 'next/link';
-import { Footer } from 'react-day-picker';
-
 
 export default function NotFound(): JSX.Element {
   return (
     <Section>
-        <NursanaLogo/>
-    <div className=" flex flex-col items-center justify-center text-center px-4">
-      <div className='text-6xl font-medium mb-4'>404</div>
-      <h1 className="text-xl font-medium text-gray-800 mb-2">Page Not Found</h1>
-      <p className="text-gray-600 mb-6">
-        The page you’re looking for doesn’t exist or has been moved.
-      </p>
-      <Link
-        href="/"
-
-      >
-       <Button>Go back to home</Button>
-      </Link>
-    </div>
-    <Footer/>
+      <NursanaLogo />
+      <div className='flex flex-col items-center justify-center px-4 text-center'>
+        <div className='mb-4 text-6xl font-medium'>404</div>
+        <h1 className='mb-2 text-xl font-medium text-gray-800'>
+          Page Not Found
+        </h1>
+        <p className='mb-6 text-gray-600'>
+          The page you’re looking for doesn’t exist or has been moved.
+        </p>
+        <Link href='/'>
+          <Button>Go back to home</Button>
+        </Link>
+      </div>
+      <Footer />
     </Section>
   );
 }
