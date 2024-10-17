7dae77c7f14781aebcba6951b8eddaa6209ed96c
crc <chinmai@aglinthq.com>
2024-10-17 15:46:59 +0530

    fix

--- app/(auth-pages)/auth/confirm/route.ts
+++ app/(auth-pages)/auth/confirm/route.ts
@@ -1,14 +1,14 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server';
 
-import { createClient } from "@/utils/supabase/server";
+import { createClient } from '@/utils/supabase/server';
 
 export async function GET(request: Request) {
   // The `/auth/callback` route is required for the server-side auth flow implemented
   // by the SSR package. It exchanges an auth code for the user's session.
   // https://supabase.com/docs/guides/auth/server-side/nextjs
   const requestUrl = new URL(request.url);
-  const code = requestUrl.searchParams.get("code");
-  const interview_id = requestUrl.searchParams.get("interview_id");
+  const code = requestUrl.searchParams.get('code');
+  const interview_id = requestUrl.searchParams.get('interview_id');
   const origin = requestUrl.origin;
 
   console.log(code, interview_id, origin);
@@ -18,12 +18,16 @@ export async function GET(request: Request) {
     await supabase.auth.exchangeCodeForSession(code);
     if (interview_id) {
       return NextResponse.redirect(
-        `${origin}/interview/${interview_id}/start-interview`
+        `${process.env.NEXT_PUBLIC_SITE_URL}/interview/${interview_id}/start-interview`,
       );
     } else {
-      return NextResponse.redirect(`${origin}/dashboard`);
+      return NextResponse.redirect(
+        `${process.env.NEXT_PUBLIC_SITE_URL}/dashboard`,
+      );
     }
   } else {
-    return NextResponse.redirect(`${origin}/user/sign-in?error=invalid_code`);
+    return NextResponse.redirect(
+      `${process.env.NEXT_PUBLIC_SITE_URL}/auth/sign-in?error=invalid_code`,
+    );
   }
 }
