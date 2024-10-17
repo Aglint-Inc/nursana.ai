e2eb971f772d646dcfa7d46f0b26be38eff65aa0
Admin Aglint <admin@aglinthq.com>
2024-10-17 10:34:56 +0530

    Dark Mode

--- app/(authenticated)/dashboard/@company/_common/components/Header/index.tsx
+++ app/(authenticated)/dashboard/@company/_common/components/Header/index.tsx
@@ -13,7 +13,7 @@ export default function DashboardHeader() {
   const { hospital } = useHospital();
 
   return (
-    <header className='fixed left-0 right-0 top-0 z-10 bg-white shadow-sm'>
+    <header className='dark:bg-blackshadow-sm fixed left-0 right-0 top-0 z-10 bg-white dark:bg-black'>
       <div className='container mx-auto flex items-center justify-between px-4 py-3'>
         <Link href='/dashboard' className='text-xl font-bold'>
           {hospital.hospital_name}
