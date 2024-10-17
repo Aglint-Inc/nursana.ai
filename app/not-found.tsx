cc6fffcbd8091c898da7f4969edf52b15d36cbea
Raimon T Simon <raimonrts@gmail.com>
2024-10-17 18:23:16 +0530

    updated 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,9 +8,10 @@ import { Button } from '@/components/ui/button';
 export default function NotFound(): JSX.Element {
   return (
     <Section>
+      <div className='h-[100vh] flex flex-col items-center justify-between pt-10'>
       <NursanaLogo />
       <div className='flex flex-col items-center justify-center px-4 text-center'>
-        <div className='mb-4 text-6xl font-medium'>404</div>
+        <div className='mb-4 text-6xl font-medium'>4<span className='text-purple-500'>0</span>4</div>
         <h1 className='mb-2 text-xl font-medium text-gray-800'>
           Page Not Found
         </h1>
@@ -22,6 +23,7 @@ export default function NotFound(): JSX.Element {
         </Link>
       </div>
       <Footer />
+      </div>
     </Section>
   );
 }
