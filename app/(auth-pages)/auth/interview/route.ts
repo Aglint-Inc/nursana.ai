a1566c6f238b015a1ad6ccc2a4b3e48f7a7e51ab
crc <chinmai@aglinthq.com>
2024-10-18 10:29:25 +0530

    fix

--- app/(auth-pages)/auth/interview/route.ts
+++ app/(auth-pages)/auth/interview/route.ts
@@ -26,9 +26,7 @@ export async function GET(request: Request) {
         console.log(
           `Redirecting to: ${origin}/interview/${interview_id}/start-interview`,
         );
-        return NextResponse.redirect(
-          `${origin}/interview/${interview_id}/start-interview`,
-        );
+        return NextResponse.redirect(`${origin}/dashboard`);
       } else {
         return NextResponse.redirect(
           `${origin}/auth/sign-in?error=no_interview_id`,
