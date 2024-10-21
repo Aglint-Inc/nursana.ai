e63ef72b8e725a0f86167105e6e0a8d7bc1e3aa1
Admin Aglint <admin@aglinthq.com>
2024-10-21 09:58:58 +0530

    Added Sidebar layout to user Profile Section

--- app/(authenticated)/profile/@user/_common/components/index.tsx
+++ app/(authenticated)/profile/@user/_common/components/index.tsx
@@ -1,9 +1,27 @@
 'use client';
 
+import { File, User } from 'lucide-react';
 import { useState } from 'react';
 
 import { useUserData } from '@/authenicated/hooks/useUserData';
-import Section from '@/components/section';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+} from '@/components/ui/sidebar';
 
 import EditProfileForm from './EditProfileForm';
 import ViewResume from './ViewResume';
@@ -12,18 +30,57 @@ function UserProfile() {
   const userDetails = useUserData();
   const [_, setEdit] = useState(false);
   return (
-    <Section>
-      <div className='flex flex-col gap-10'>
-        <EditProfileForm setEdit={setEdit} />
-
-        {/* {edit ? (
-          <EditProfileForm setEdit={setEdit} />
-        ) : (
-          <ViewProfileDetails setEdit={setEdit} />
-        )} */}
-        <ViewResume data={userDetails.resume?.structured_resume} />
-      </div>
-    </Section>
+    <div className='container flex max-w-screen-xl'>
+      <SidebarProvider className='items-start'>
+        <Sidebar collapsible='none' className='hidden md:flex'>
+          <SidebarContent>
+            <SidebarGroup>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton asChild isActive={true}>
+                      <a href='/profile'>
+                        <User />
+                        <span>Basic Information</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton asChild isActive={true}>
+                      <a href='/profile'>
+                        <File />
+                        <span>Resume</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </SidebarGroup>
+          </SidebarContent>
+        </Sidebar>
+        <main className='flex flex-1 flex-col overflow-hidden'>
+          <header className='flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12'>
+            <div className='flex items-center gap-2 px-4'>
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <BreadcrumbItem className='hidden md:block'>
+                    <BreadcrumbLink href='#'>Profile</BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator className='hidden md:block' />
+                  <BreadcrumbItem>
+                    <BreadcrumbPage>Basic Information</BreadcrumbPage>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+            </div>
+          </header>
+          <div className='flex flex-1 flex-col gap-4 overflow-y-auto p-4 pt-0'>
+            <EditProfileForm setEdit={setEdit} />
+            <ViewResume data={userDetails.resume?.structured_resume} />
+          </div>
+        </main>
+      </SidebarProvider>
+    </div>
   );
 }
 
