446a34faf5d5dc527b38b680608d09af346fab6e
Punith G <punithg@aglinthq.com>
2024-10-21 15:36:27 +0530

    sidebar inset

--- app/(authenticated)/(hospital)/campaigns/[campaign]/page.tsx
+++ app/(authenticated)/(hospital)/campaigns/[campaign]/page.tsx
@@ -1,9 +1,17 @@
 'use client';
 
 import { Body } from '@/campaign/components/Body';
+import { Table } from '@/campaign/components/Table';
+import { SidebarInset } from '@/components/ui/sidebar';
 
 const Page = () => {
-  return <Body>LLLL</Body>;
+  return (
+    <SidebarInset>
+      <Body>
+        <Table />
+      </Body>
+    </SidebarInset>
+  );
 };
 
 export default Page;
