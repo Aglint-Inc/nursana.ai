70e076d4ca08a7d39de782ba4a5307859b7523fa
Punith G <punithg@aglinthq.com>
2024-10-22 21:39:33 +0530

    loaders

--- app/(authenticated)/(hospital)/campaigns/_common/components/Loading.tsx
+++ app/(authenticated)/(hospital)/campaigns/_common/components/Loading.tsx
@@ -1,11 +1,14 @@
 import { Skeleton } from '@/campaigns/components/DataTable/skeleton';
+import { SidebarInset } from '@/components/ui/sidebar';
 import { Skeleton as DefaultSkeleton } from '@/components/ui/skeleton';
 
 export const Loading = () => {
   return (
-    <div className='flex w-full flex-col gap-2 p-4'>
-      <DefaultSkeleton className='h-12 w-full' />
-      <Skeleton />
-    </div>
+    <SidebarInset>
+      <div className='flex w-full flex-col gap-2 p-4'>
+        <DefaultSkeleton className='h-12 w-full' />
+        <Skeleton />
+      </div>
+    </SidebarInset>
   );
 };
