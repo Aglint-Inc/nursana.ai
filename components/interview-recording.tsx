46e94ad1aa2b5d28f91046c7fb9fa52ef327d7db
Raimon T Simon <raimonrts@gmail.com>
2024-10-22 14:50:37 +0530

    interview taking screen button update

--- components/interview-recording.tsx
+++ components/interview-recording.tsx
@@ -7,6 +7,7 @@ import { AspectRatio } from '@/components/ui/aspect-ratio';
 import { Card, CardContent } from '@/components/ui/card';
 
 import { Button } from './ui/button';
+import { Separator } from './ui/separator';
 
 function InterviewRecording({
   handleStopInterview,
@@ -102,7 +103,7 @@ function InterviewRecording({
             {isInterviewStarted && (
               <>
                 <div className='absolute bottom-0 left-0 flex w-full justify-center gap-2 bg-gradient-to-t from-[#00000050] to-transparent py-4'>
-                  <div className='flex h-[36px] items-center justify-center rounded-md bg-white px-4 text-sm text-red-600'>
+                  {/* <div className='flex h-[36px] items-center justify-center rounded-md px-4 text-sm text-red-600'>
                     <StopCircle
                       onClick={() => {
                         setShowStopInterviewModal(true);
@@ -115,6 +116,25 @@ function InterviewRecording({
                     <span className='ml-2'>
                       {formatTime(timer)}/{formatTime(interviewDuration * 60)}
                     </span>
+                  </div> */}
+                  <div className='flex gap-3 rounded-full bg-black p-2'>
+                    <div
+                      className='flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-white/20 p-0 text-white duration-200 hover:bg-red-500 hover:text-white'
+                      onClick={() => {
+                        setShowStopInterviewModal(true);
+                      }}
+                    >
+                      <StopCircle size={24} strokeWidth={1.5} />
+                    </div>
+                    <Separator
+                      orientation='vertical'
+                      className='rounded-full bg-gray-700'
+                    />
+                    <div className='flex h-full items-center'>
+                      <span className='mr-2 text-white select-none'>
+                        {formatTime(timer)}/{formatTime(interviewDuration * 60)}
+                      </span>
+                    </div>
                   </div>
                 </div>
               </>
