6668cebb717825b9f6d61ac20a0cc2f57e42868c
ChandraAglint <chandra@aglinthq.com>
2024-10-24 09:09:53 +0530

    fix (#46)
    Co-authored-by: ChandrakumarV <chandrakumar252004@gmail.com>

--- components/sidebar/app-sidebar.tsx
+++ components/sidebar/app-sidebar.tsx
@@ -5,12 +5,11 @@ import {
   ChevronsUpDown,
   Command,
   File,
-  List,
   LogOut,
-  Search,
   SquarePen,
   Tags,
 } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 import React, { type ReactNode } from 'react';
 
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
@@ -43,111 +42,29 @@ const data = {
     avatar: '/avatars/shadcn.jpg',
   },
   navMain: [
-    {
-      title: 'Lists',
-      url: '/list',
-      icon: List,
-      isActive: false,
-    },
-    {
-      title: 'Search',
-      url: '/search',
-      icon: Search,
-      isActive: false,
-    },
+    // {
+    //   title: 'Lists',
+    //   url: '/list',
+    //   icon: List,
+    //   isActive: false,
+    // },
+    // {
+    //   title: 'Search',
+    //   url: '/search',
+    //   icon: Search,
+    //   isActive: false,
+    // },
     {
       title: 'Campaigns',
-      url: '/campaign',
+      url: '/campaigns',
       icon: Tags,
       isActive: true,
     },
     {
       title: 'Templates',
-      url: '/template',
+      url: '/templates',
       icon: File,
       isActive: false,
-    },
-  ],
-  mails: [
-    {
-      name: 'William Smith',
-      email: 'williamsmith@example.com',
-      subject: 'Meeting Tomorrow',
-      date: '09:34 AM',
-      teaser:
-        'Hi team, just a reminder about our meeting tomorrow at 10 AM.\nPlease come prepared with your project updates.',
-    },
-    {
-      name: 'Alice Smith',
-      email: 'alicesmith@example.com',
-      subject: 'Re: Project Update',
-      date: 'Yesterday',
-      teaser:
-        "Thanks for the update. The progress looks great so far.\nLet's schedule a call to discuss the next steps.",
-    },
-    {
-      name: 'Bob Johnson',
-      email: 'bobjohnson@example.com',
-      subject: 'Weekend Plans',
-      date: '2 days ago',
-      teaser:
-        "Hey everyone! I'm thinking of organizing a team outing this weekend.\nWould you be interested in a hiking trip or a beach day?",
-    },
-    {
-      name: 'Emily Davis',
-      email: 'emilydavis@example.com',
-      subject: 'Re: Question about Budget',
-      date: '2 days ago',
-      teaser:
-        "I've reviewed the budget numbers you sent over.\nCan we set up a quick call to discuss some potential adjustments?",
-    },
-    {
-      name: 'Michael Wilson',
-      email: 'michaelwilson@example.com',
-      subject: 'Important Announcement',
-      date: '1 week ago',
-      teaser:
-        "Please join us for an all-hands meeting this Friday at 3 PM.\nWe have some exciting news to share about the company's future.",
-    },
-    {
-      name: 'Sarah Brown',
-      email: 'sarahbrown@example.com',
-      subject: 'Re: Feedback on Proposal',
-      date: '1 week ago',
-      teaser:
-        "Thank you for sending over the proposal. I've reviewed it and have some thoughts.\nCould we schedule a meeting to discuss my feedback in detail?",
-    },
-    {
-      name: 'David Lee',
-      email: 'davidlee@example.com',
-      subject: 'New Project Idea',
-      date: '1 week ago',
-      teaser:
-        "I've been brainstorming and came up with an interesting project concept.\nDo you have time this week to discuss its potential impact and feasibility?",
-    },
-    {
-      name: 'Olivia Wilson',
-      email: 'oliviawilson@example.com',
-      subject: 'Vacation Plans',
-      date: '1 week ago',
-      teaser:
-        "Just a heads up that I'll be taking a two-week vacation next month.\nI'll make sure all my projects are up to date before I leave.",
-    },
-    {
-      name: 'James Martin',
-      email: 'jamesmartin@example.com',
-      subject: 'Re: Conference Registration',
-      date: '1 week ago',
-      teaser:
-        "I've completed the registration for the upcoming tech conference.\nLet me know if you need any additional information from my end.",
-    },
-    {
-      name: 'Sophia White',
-      email: 'sophiawhite@example.com',
-      subject: 'Team Dinner',
-      date: '1 week ago',
-      teaser:
-        "To celebrate our recent project success, I'd like to organize a team dinner.\nAre you available next Friday evening? Please let me know your preferences.",
     },
   ],
 };
@@ -162,20 +79,10 @@ export function AppSidebar({
 }: AppSidebarProps) {
   const [activeItem, setActiveItem] = React.useState(data.navMain[0]);
 
-  function setMails(
-    _arg0: {
-      name: string;
-      email: string;
-      subject: string;
-      date: string;
-      teaser: string;
-    }[],
-  ) {
-    throw new Error('Function not implemented.');
-  }
+  const { push } = useRouter();
 
-  function setOpen(_arg0: boolean) {
-    throw new Error('Function not implemented.');
+  function setOpen(url: string) {
+    push(url);
   }
 
   return (
@@ -219,14 +126,8 @@ export function AppSidebar({
                       }}
                       onClick={() => {
                         setActiveItem(item);
-                        const mail = data.mails.sort(() => Math.random() - 0.5);
-                        setMails(
-                          mail.slice(
-                            0,
-                            Math.max(5, Math.floor(Math.random() * 10) + 1),
-                          ),
-                        );
-                        setOpen(true);
+
+                        setOpen(item.url);
                       }}
                       isActive={activeItem.title === item.title}
                       className='px-2.5 md:px-2'
