91b43720ff4cbec7074b59d92220dff52c9e2ec4
Dheeraj Kumar <127676975+dheerajaglinthq@users.noreply.github.com>
2024-10-26 20:05:22 +0530

    fix: lint error

--- edge_function/process_resume_v1/src/utils.ts
+++ edge_function/process_resume_v1/src/utils.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { SupabaseClient } from '@supabase/supabase-js';
+import { type SupabaseClient } from '@supabase/supabase-js';
 
 export type ErrorType =
   | 'SYSTEM_ERROR'
