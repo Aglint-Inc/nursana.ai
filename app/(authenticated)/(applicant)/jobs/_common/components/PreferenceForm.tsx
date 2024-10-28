19015cf20403a19fd5afce0e73d4f8cef14ffd24
Raimon T Simon <raimonrts@gmail.com>
2024-10-28 18:08:45 +0530

    updated prefeencces

--- app/(authenticated)/(applicant)/jobs/_common/components/PreferenceForm.tsx
+++ app/(authenticated)/(applicant)/jobs/_common/components/PreferenceForm.tsx
@@ -4,6 +4,7 @@ import {
   JOB_TYPES,
   TRAVEL_PREFERENCES,
 } from 'app/(authenticated)/(applicant)/profile/_common/constant';
+import { CheckCircle2 } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { Label } from 'recharts';
 import { type z } from 'zod';
@@ -22,6 +23,7 @@ import {
   useUserData,
 } from '@/applicant/hooks/useUserData';
 import { useLocationsList } from '@/authenticated/hooks/useLocationsList';
+import { Loader } from '@/common/components/Loader';
 import { UIMultiSelect } from '@/common/components/UIMultiSelect';
 import { Button } from '@/components/ui/button';
 import {
@@ -117,131 +119,144 @@ function PreferenceForm() {
               profile now. It takes only a few minutes and will help us match
               you with the best opportunities.
             </div>
-            <div className='grid grid-cols-2 gap-2 mt-4'>
-                <div className='col-span-2'>
-                  <div>
-                    <Label>Preferred Travel Preference</Label>
-                    <Select
-                      onValueChange={(
-                        value: z.infer<typeof travelPreferrenceSchema>,
-                      ) => {
-                        handleTravelPreferenceChange(value);
-                        setTravelPreference(value);
-                      }}
-                      value={travelPreference}
-                    >
-                      <SelectTrigger id='travel_preference'>
-                        <SelectValue placeholder='Select preferred travel preference' />
-                      </SelectTrigger>
-                      <SelectContent>
-                        {TRAVEL_PREFERENCES.map((item) => (
-                          <SelectItem key={item} value={item}>
-                            {capitalizeFirstLetter(item.split('-').join(' '))}
-                          </SelectItem>
-                        ))}
-                      </SelectContent>
-                    </Select>
-                  </div>
+            <div className='mt-4 grid grid-cols-2 gap-2'>
+              <div className='col-span-2'>
+                <div>
+                  <Label>Preferred Travel Preference</Label>
+                  <Select
+                    onValueChange={(
+                      value: z.infer<typeof travelPreferrenceSchema>,
+                    ) => {
+                      handleTravelPreferenceChange(value);
+                      setTravelPreference(value);
+                    }}
+                    value={travelPreference}
+                  >
+                    <SelectTrigger id='travel_preference'>
+                      <SelectValue placeholder='Select preferred travel preference' />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {TRAVEL_PREFERENCES.map((item) => (
+                        <SelectItem key={item} value={item}>
+                          {capitalizeFirstLetter(item.split('-').join(' '))}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
                 </div>
-                <div className='col-span-2'>
-                  <div>
-                    <Label>Preferred Job Types</Label>
-                    <UIMultiSelect
-                      onDelete={(value) => {
-                        deletePreferredJobTypes({
-                          job_type: value as z.infer<typeof jobTypesSchema>,
-                        });
-                      }}
-                      listItems={JOB_TYPES.map((item) => ({
-                        label: capitalizeFirstLetter(item),
-                        value: item,
-                      }))}
-                      onChange={(_values, value) => {
-                        createPreferredJobTypes({
-                          job_type: value as z.infer<typeof jobTypesSchema>,
-                        });
-                      }}
-                      defaultValue={
-                        preferredJobTypes.map(
-                          (item) => item.job_type,
-                        ) as string[]
-                      }
-                      level='Job Types'
-                    />
-                  </div>
-                </div>
-                <div className='col-span-2'>
-                  <div>
-                    <Label>Preferred Job Titles</Label>
-                    <UIMultiSelect
-                      onDelete={(value) => {
-                        deletePreferredJobTitles({
-                          job_title: value as z.infer<typeof jobTitlesSchema>,
-                        });
-                      }}
-                      listItems={JOB_TITLES.map((item) => ({
-                        label: capitalizeFirstLetter(item),
-                        value: item,
-                      }))}
-                      onChange={(_values, value) => {
-                        createPreferredJobTitles({
-                          job_title: value as z.infer<typeof jobTitlesSchema>,
-                        });
-                      }}
-                      defaultValue={
-                        preferredJobTitle.map(
-                          (item) => item.job_title,
-                        ) as string[]
-                      }
-                      level='Job Titles'
-                    />
-                  </div>
-                </div>
-                <div className='col-span-2'>
-                  <div>
-                    <Label>Preferred Locations</Label>
-                    <UIMultiSelect
-                      onDelete={(value) => {
-                        deletePreferredLocations({
-                          location_id: value,
-                        });
-                      }}
-                      listItems={locationList.map((item) => ({
-                        label: capitalizeFirstLetter(item.level),
-                        value: item.id,
-                      }))}
-                      onChange={(_values, value) => {
-                        createPreferredLocations({
-                          location_id: value,
-                        });
-                      }}
-                      defaultValue={preferredLocations.map(
-                        (item) => item.location_id,
-                      )}
-                      level='Preferred Locations'
-                    />
-                  </div>
-                </div>
-                {isCompletePreferenceForm && (
-            <div className='col-span-2 flex items-start'>
-              <Button onClick={() => {
-                  setLocalStoragePreference(true);
-                }}>
-                Close
-              </Button>
-              <div className='hidden'>
-                  <span className='text-muted-foreground'>
-                    {isSaving ? 'Saving...' : ''}
-                  </span>
-                  <p>
-                    {!isSaving && isCompletePreferenceForm
-                      ? 'Preference Complete : ✅ '
-                      : ''}
-                  </p>
-                </div>
-            </div>
-          )}
               </div>
+              <div className='col-span-2'>
+                <div>
+                  <Label>Preferred Job Types</Label>
+                  <UIMultiSelect
+                    onDelete={(value) => {
+                      deletePreferredJobTypes({
+                        job_type: value as z.infer<typeof jobTypesSchema>,
+                      });
+                    }}
+                    listItems={JOB_TYPES.map((item) => ({
+                      label: capitalizeFirstLetter(item),
+                      value: item,
+                    }))}
+                    onChange={(_values, value) => {
+                      createPreferredJobTypes({
+                        job_type: value as z.infer<typeof jobTypesSchema>,
+                      });
+                    }}
+                    defaultValue={
+                      preferredJobTypes.map((item) => item.job_type) as string[]
+                    }
+                    level='Job Types'
+                  />
+                </div>
+              </div>
+              <div className='col-span-2'>
+                <div>
+                  <Label>Preferred Job Titles</Label>
+                  <UIMultiSelect
+                    onDelete={(value) => {
+                      deletePreferredJobTitles({
+                        job_title: value as z.infer<typeof jobTitlesSchema>,
+                      });
+                    }}
+                    listItems={JOB_TITLES.map((item) => ({
+                      label: capitalizeFirstLetter(item),
+                      value: item,
+                    }))}
+                    onChange={(_values, value) => {
+                      createPreferredJobTitles({
+                        job_title: value as z.infer<typeof jobTitlesSchema>,
+                      });
+                    }}
+                    defaultValue={
+                      preferredJobTitle.map(
+                        (item) => item.job_title,
+                      ) as string[]
+                    }
+                    level='Job Titles'
+                  />
+                </div>
+              </div>
+              <div className='col-span-2'>
+                <div>
+                  <Label>Preferred Locations</Label>
+                  <UIMultiSelect
+                    onDelete={(value) => {
+                      deletePreferredLocations({
+                        location_id: value,
+                      });
+                    }}
+                    listItems={locationList.map((item) => ({
+                      label: capitalizeFirstLetter(item.level),
+                      value: item.id,
+                    }))}
+                    onChange={(_values, value) => {
+                      createPreferredLocations({
+                        location_id: value,
+                      });
+                    }}
+                    defaultValue={preferredLocations.map(
+                      (item) => item.location_id,
+                    )}
+                    level='Preferred Locations'
+                  />
+                </div>
+              </div>
+              {isCompletePreferenceForm && (
+                <div className='col-span-2 flex items-center justify-between'>
+                  <div className=''>
+                    <span className='text-muted-foreground'>
+                      {isSaving ? (
+                        <div className='grid grid-cols-[max-content_1fr] items-center gap-2 '>
+                          <Loader />
+                          <p>Saving preferences...</p>
+                        </div>
+                      ) : (
+                       ''
+                      )}
+                    </span>
+                    <p>
+                      {!isSaving && isCompletePreferenceForm ? (
+                        <div className='flex items-center gap-2 text-green-600'>
+                          <CheckCircle2 size={16} />
+                          <p>Preferences Saved</p>
+                        </div>
+                      ) : (
+                        ''
+                      )}
+                    </p>
+                  </div>
+                  <Button
+                  size={'sm'}
+                    onClick={() => {
+                      setLocalStoragePreference(true);
+                    }}
+                  >
+                    Close
+                  </Button>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       )}
