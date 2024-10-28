82db2dce09fd2279b45cce84bb237046e0a0d087
Punith G <punithg@aglinthq.com>
2024-10-28 11:39:07 +0530

    remove useless prefetch

--- app/(authenticated)/(agency)/layout.tsx
+++ app/(authenticated)/(agency)/layout.tsx
@@ -1,6 +1,6 @@
 import { unstable_noStore } from 'next/cache';
 import type { PropsWithChildren } from 'react';
-import { api, HydrateClient } from 'trpc/server';
+import { HydrateClient } from 'trpc/server';
 
 import { AgencyEditProvider } from '@/agency/hooks/useAgencyEdit';
 import type { PageProps } from '@/agency/types';
@@ -9,8 +9,8 @@ import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 
 const Layout = (props: PropsWithChildren<PageProps>) => {
   unstable_noStore();
-  void api.authenticated.agency.read.prefetch();
-  void api.authenticated.agency.user.prefetch();
+  // void api.authenticated.agency.read.prefetch();
+  // void api.authenticated.agency.user.prefetch();
   return (
     <HydrateClient>
       <AgencyEditProvider>
