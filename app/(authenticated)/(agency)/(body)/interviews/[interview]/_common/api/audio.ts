c5bde6ea574bd1551e8672652358b198bd849390
Punith G <punithg@aglinthq.com>
2024-10-28 20:11:43 +0530

    audio/video hooks

---
+++ app/(authenticated)/(agency)/(body)/interviews/[interview]/_common/api/audio.ts
@@ -0,0 +1,60 @@
+import 'server-only';
+
+import { TRPCError } from '@trpc/server';
+
+import {
+  type InterviewProcedure,
+  interviewProcedure,
+} from '@/interview/utils/interviewProcedure';
+import { type ProcedureDefinition } from '@/server/api/trpc';
+import { createPublicClient } from '@/server/db';
+
+const query = async ({ ctx, input }: InterviewProcedure) => {
+  const db = createPublicClient();
+
+  const data = (
+    await db
+      .from('interview')
+      .select(
+        'interview_analysis!interview_analysis_interview_id_fkey!inner(audio_url)',
+      )
+      .eq('id', input.id)
+      .eq('agency_id', ctx.agency.id)
+      .single()
+      .throwOnError()
+  ).data;
+
+  if (!data?.interview_analysis.audio_url)
+    throw new TRPCError({
+      code: 'NOT_FOUND',
+      message: 'Audio not found',
+    });
+
+  const url = data.interview_analysis.audio_url;
+  const filename = url.split('audio/')[1];
+
+  if (!filename)
+    throw new TRPCError({
+      code: 'INTERNAL_SERVER_ERROR',
+      message: 'Invalid filename',
+    });
+
+  const response = await db.storage
+    .from('audio')
+    .createSignedUrl(filename, process.env.NEXT_PUBLIC_PUBLIC_URL_EXP);
+
+  if (response.error || !response.data) {
+    throw new TRPCError({
+      code: 'INTERNAL_SERVER_ERROR',
+      message: 'Error signing url',
+    });
+  }
+
+  const { signedUrl } = response.data;
+
+  return signedUrl;
+};
+
+export const audio = interviewProcedure.query(query);
+
+export type Audio = ProcedureDefinition<typeof audio>;
--- app/(authenticated)/(agency)/(body)/interviews/[interview]/_common/api/index.ts
+++ app/(authenticated)/(agency)/(body)/interviews/[interview]/_common/api/index.ts
@@ -2,12 +2,16 @@ import { createTRPCRouter } from '@/server/api/trpc';
 
 import { analysis } from './analysis';
 import { applicant } from './applicant';
+import { audio } from './audio';
 import { read } from './read';
 import { resume } from './resume';
+import { video } from './video';
 
 export const interview = createTRPCRouter({
   analysis,
   applicant,
+  audio,
   read,
   resume,
+  video,
 });
