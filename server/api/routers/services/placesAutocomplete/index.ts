e6f4c9ec4e033390ca72e1204a4900d8b8c2987a
Dileep BC <dileep@aglinthq.com>
2024-10-29 15:14:10 +0530

    fix : pref location

--- server/api/routers/services/placesAutocomplete/index.ts
+++ server/api/routers/services/placesAutocomplete/index.ts
@@ -6,29 +6,31 @@ import { z } from 'zod';
 import { type PublicProcedure, publicProcedure } from '@/server/api/trpc';
 
 const schema = z.object({ text_query: z.string().nullish() });
-const API_URL = 'https://maps.googleapis.com/maps/api/place/autocomplete/json';
+const API_URL = 'https://places.googleapis.com/v1/places:autocomplete';
 
 type ResponsePrediction = {
-  description: string;
-  matched_substrings: {
-    length: number;
-    offset: number;
-  }[];
-  place_id: string;
-  reference: string;
-  structured_formatting: {
-    main_text: string;
-    main_text_matched_substrings: {
-      length: number;
-      offset: number;
-    }[];
-    secondary_text: string;
+  placePrediction: {
+    place: string;
+    placeId: string;
+    text: {
+      text: string;
+      matches: {
+        endOffset: number;
+      }[];
+    };
+    structuredFormat: {
+      mainText: {
+        text: string;
+        matches: {
+          endOffset: number;
+        }[];
+      };
+      secondaryText: {
+        text: string;
+      };
+    };
+    types: string[];
   };
-  terms: {
-    offset: number;
-    value: string;
-  }[];
-  types: string[];
 };
 
 const query = async ({
@@ -37,20 +39,26 @@ const query = async ({
   if (!text_query || text_query?.length == 0) {
     return [];
   }
-  const { data } = await axios.get(API_URL, {
-    params: {
+  const { data } = await axios.post(
+    API_URL,
+    {
       input: text_query,
-      types: 'geocode',
-      key: process.env.GOOGLE_PLACES_API_KEY,
     },
-  });
-  const predictions = data.predictions as ResponsePrediction[];
+    {
+      headers: {
+        'X-Goog-Api-Key': process.env.GOOGLE_PLACES_API_KEY,
+      },
+    },
+  );
+  const predictions = data.suggestions as ResponsePrediction[];
 
-  let response: Pick<ResponsePrediction, 'description' | 'place_id'>[] =
-    predictions.map((pred) => ({
-      description: pred.description,
-      place_id: pred.place_id,
-    }));
+  let response: {
+    description: string;
+    place_id: string;
+  }[] = predictions.map(({ placePrediction }) => ({
+    description: placePrediction.text.text,
+    place_id: placePrediction.placeId,
+  }));
   if (response.length === 0) {
     response = [
       {
--- server/api/routers/user/create/preferred-job-locations.ts
+++ server/api/routers/user/create/preferred-job-locations.ts
@@ -1,30 +1,105 @@
+/* eslint-disable no-unused-vars */
 import 'server-only';
 
+import axios from 'axios';
 import { z } from 'zod';
 
 import { type ApplicantProcedure, applicantProcedure } from '@/server/api/trpc';
 import { createPublicClient } from '@/server/db';
+const API_URL = 'https://places.googleapis.com/v1/places/';
 
 export const schema = z.object({
-  location_id: z.string(),
+  maps_place_id: z.string(),
+  place_description: z.string(),
 });
+type AddressComponentType = {
+  long_name: string;
+  short_name: string;
+  types: AddressType[];
+};
 
+enum AddressType {
+  SublocalityLevel3 = 'sublocality_level_3',
+  SublocalityLevel2 = 'sublocality_level_2',
+  SublocalityLevel1 = 'sublocality_level_1',
+  Locality = 'locality',
+  AdministrativeAreaLevel3 = 'administrative_area_level_3',
+  AdministrativeAreaLevel2 = 'administrative_area_level_2',
+  AdministrativeAreaLevel1 = 'administrative_area_level_1',
+  Country = 'country',
+  PostalCode = 'postal_code',
+  Political = 'political',
+  Sublocality = 'sublocality',
+}
+
+type AddressResult = {
+  addressComponents: AddressComponentType[];
+};
 const mutation = async ({
   ctx: { user_id },
-  input: { location_id },
+  input: { maps_place_id, place_description },
 }: ApplicantProcedure<typeof schema>) => {
   const supabase = createPublicClient();
-  const { data } = await supabase
-    .from('preferred_locations')
-    .insert({
-      applicant_id: user_id,
-      location_id: location_id,
+
+  const { city, country, postal_code, state } =
+    await getPlaceDetails(maps_place_id);
+  const { data: location_list } = await supabase
+    .from('locations_list')
+    .upsert({
+      city: city.long_name,
+      country: country.long_name,
+      level: place_description,
+      state: state.long_name,
     })
     .select()
+    .single()
     .throwOnError();
-  return data;
+  if (!location_list) {
+    throw Error('locations list undefined');
+  }
+  const { data } = await supabase
+    .from('preferred_locations')
+    .upsert({
+      applicant_id: user_id,
+      location_id: location_list?.id,
+    })
+    .select()
+    .single()
+    .throwOnError();
 };
 
 export const createPreferredJobLocations = applicantProcedure
   .input(schema)
   .mutation(mutation);
+
+const getPlaceDetails = async (place_id: string) => {
+  const { data } = await axios.get(API_URL + '/' + place_id, {
+    headers: {
+      'X-Goog-Api-Key': process.env.GOOGLE_PLACES_API_KEY,
+      'X-Goog-FieldMask': '*',
+    },
+  });
+  const address = data as AddressResult;
+  const addressComponents = address.addressComponents;
+  const city = addressComponents.find((component) =>
+    component.types.includes(AddressType.Locality),
+  );
+  const state = addressComponents.find((component) =>
+    component.types.includes(AddressType.AdministrativeAreaLevel1),
+  );
+  const country = addressComponents.find((component) =>
+    component.types.includes(AddressType.Country),
+  );
+  const postal_code = addressComponents.find((component) =>
+    component.types.includes(AddressType.PostalCode),
+  );
+  if (!city || !state || !country || !postal_code) {
+    throw new Error('Invalid place_id');
+  }
+  return {
+    city,
+    state,
+    country,
+    postal_code,
+  };
+};
