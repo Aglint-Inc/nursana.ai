319f0c0a6686e8da628d35d20d137b726baa5464
Punith G <punithg@aglinthq.com>
2024-10-30 12:31:44 +0530

    custom schema setup

---
+++ app/_common/types.ts
@@ -0,0 +1,31 @@
+export type CustomizableTypes<T extends 'Array' | 'Object'> = T extends 'Array'
+  ? Record<string, unknown>[]
+  : Record<string, unknown>;
+
+export type Custom<
+  T extends CustomizableTypes<'Array'> | CustomizableTypes<'Object'>,
+  U extends T extends CustomizableTypes<'Array'>
+    ? Partial<{ [_id in keyof T[number]]: unknown }>
+    : Partial<{ [_id in keyof T]: unknown }>,
+> =
+  T extends CustomizableTypes<'Array'>
+    ? (Omit<T[number], keyof U> &
+        Required<Pick<U, Extract<keyof RequiredOnly<T[number]>, keyof U>>> &
+        Partial<Pick<U, Extract<keyof PartialOnly<T[number]>, keyof U>>>)[]
+    : T extends CustomizableTypes<'Object'>
+      ? Omit<T, keyof U> &
+          Required<Pick<U, Extract<keyof RequiredOnly<T>, keyof U>>> &
+          Partial<Pick<U, Extract<keyof PartialOnly<T>, keyof U>>>
+      : never;
+
+type RequiredOnly<T extends CustomizableTypes<'Object'>> = Pick<
+  T,
+  {
+    [id in keyof T]-?: undefined extends T[id] ? never : id;
+  }[keyof T]
+>;
+
+type PartialOnly<T extends CustomizableTypes<'Object'>> = Omit<
+  T,
+  keyof RequiredOnly<T>
+>;
