d909abb99e49b573d193ab674d714112c7033b35
Punith G <punithg@aglinthq.com>
2024-10-30 15:02:38 +0530

    short-circuit unecessary type checks

--- app/_common/types.ts
+++ app/_common/types.ts
@@ -2,20 +2,35 @@ export type CustomizableTypes<T extends 'Array' | 'Object'> = T extends 'Array'
   ? Record<string, unknown>[]
   : Record<string, unknown>;
 
+export type Options<T extends CustomizableTypes<'Object'>> = Partial<{
+  [_id in keyof T]: unknown;
+}>;
+
+export type CustomObject<
+  T extends CustomizableTypes<'Object'>,
+  U extends Options<T>,
+> = keyof U extends infer R
+  ? Nullable<
+      T,
+      //@ts-expect-error
+      Omit<T, R> &
+        Required<Pick<U, Extract<keyof RequiredOnly<T>, R>>> &
+        Partial<Pick<U, Extract<keyof PartialOnly<T>, R>>>
+    >
+  : never;
+
 export type Custom<
-  T extends CustomizableTypes<'Array'> | CustomizableTypes<'Object'>,
-  U extends T extends CustomizableTypes<'Array'>
-    ? Partial<{ [_id in keyof T[number]]: unknown }>
-    : Partial<{ [_id in keyof T]: unknown }>,
+  T extends CustomizableTypes<'Object'> | CustomizableTypes<'Array'>,
+  U extends T extends CustomizableTypes<'Object'>
+    ? Options<T>
+    : //@ts-expect-error
+      Options<T[number]>,
 > =
-  T extends CustomizableTypes<'Array'>
-    ? (Omit<T[number], keyof U> &
-        Required<Pick<U, Extract<keyof RequiredOnly<T[number]>, keyof U>>> &
-        Partial<Pick<U, Extract<keyof PartialOnly<T[number]>, keyof U>>>)[]
-    : T extends CustomizableTypes<'Object'>
-      ? Omit<T, keyof U> &
-          Required<Pick<U, Extract<keyof RequiredOnly<T>, keyof U>>> &
-          Partial<Pick<U, Extract<keyof PartialOnly<T>, keyof U>>>
+  T extends CustomizableTypes<'Object'>
+    ? //@ts-expect-error
+      CustomObject<T, U>
+    : T extends CustomizableTypes<'Array'>
+      ? CustomObject<T[number], U>
       : never;
 
 type RequiredOnly<T extends CustomizableTypes<'Object'>> = Pick<
@@ -29,3 +44,17 @@ type PartialOnly<T extends CustomizableTypes<'Object'>> = Omit<
   T,
   keyof RequiredOnly<T>
 >;
+
+type Nullable<
+  T extends CustomizableTypes<'Object'>,
+  U extends Options<T>,
+  //@ts-expect-error
+> = NullableOnly<T> extends infer R ? Omit<U, R> & Nullify<Pick<U, R>> : never;
+
+type NullableOnly<T extends CustomizableTypes<'Object'>> = {
+  [id in keyof T]: null extends T[id] ? id : never;
+}[keyof T];
+
+type Nullify<T extends CustomizableTypes<'Object'>> = {
+  [id in keyof T]: T[id] | null;
+};
--- server/db/schema/tables/index.ts
+++ server/db/schema/tables/index.ts
@@ -1,6 +1,7 @@
 import type { Custom, CustomizableTypes } from '@/app/types';
 
 import type { Database } from '../default';
+import type { AgencyTable } from './agency';
 
 type DatabaseTables = Database['public']['Tables'];
 type DatabaseTableInsert<T extends keyof DatabaseTables> =
@@ -9,25 +10,33 @@ type DatabaseTableRow<T extends keyof DatabaseTables> =
   DatabaseTables[T]['Row'];
 type DatabaseTableUpdate<T extends keyof DatabaseTables> =
   DatabaseTables[T]['Update'];
-type DatabaseTableRelationships<T extends keyof DatabaseTables> =
-  DatabaseTables[T]['Relationships'];
 
 export type TableType<
   T extends keyof DatabaseTables,
-  U extends DatabaseTableRow<T> extends CustomizableTypes<'Array'>
-    ? { [_id in keyof Partial<DatabaseTableRow<T>[number]>]: any }
-    : { [_id in keyof Partial<DatabaseTableRow<T>>]: any },
+  U extends DatabaseTableRow<T> extends infer R
+    ? R extends CustomizableTypes<'Object'>
+      ? { [_id in keyof Partial<R>]: unknown }
+      : //@ts-expect-error
+        { [_id in keyof Partial<R[number]>]: unknown }
+    : never,
 > = Required<
   Custom<
     DatabaseTables[T],
     //@ts-expect-error
     {
+      //@ts-expect-error
       Row: Custom<DatabaseTableRow<T>, U>;
+      //@ts-expect-error
       Insert: Custom<DatabaseTableInsert<T>, U>;
+      //@ts-expect-error
       Update: Custom<DatabaseTableUpdate<T>, U>;
-      Relationships: DatabaseTableRelationships<T>;
     }
   >
 >;
 
-export type Tables = Custom<DatabaseTables, {}>;
+export type Tables = Custom<
+  DatabaseTables,
+  {
+    agency: AgencyTable;
+  }
+>;
