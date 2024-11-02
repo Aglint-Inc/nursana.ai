3ff3618e782bf2abac78913373b1130240188cf8
Admin Aglint <admin@aglinthq.com>
2024-11-02 12:19:52 +0530

    Reverted Change

--- app/api/score_resume/util.ts
+++ app/api/score_resume/util.ts
@@ -9,7 +9,7 @@ export async function score<T extends z.ZodSchema>(
   schema: T,
 ) {
   const { object, usage } = await generateObject<z.infer<T>>({
-    model: openai('gpt-4o'),
+    model: openai('gpt-4o-mini'),
     system: systemPrompt,
     prompt: data,
     schema,
