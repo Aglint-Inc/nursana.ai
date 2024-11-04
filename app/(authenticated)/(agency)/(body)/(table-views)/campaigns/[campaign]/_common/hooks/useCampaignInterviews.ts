e45d9fad4026b0ee1a34d376469b7d7c0f999a18
Punith G <punithg@aglinthq.com>
2024-11-04 17:26:48 +0530

    redundant server filter

--- app/(authenticated)/(agency)/(body)/(table-views)/campaigns/[campaign]/_common/hooks/useCampaignInterviews.ts
+++ app/(authenticated)/(agency)/(body)/(table-views)/campaigns/[campaign]/_common/hooks/useCampaignInterviews.ts
@@ -1,4 +1,4 @@
-import { useDeferredValue } from 'react';
+// import { useDeferredValue } from 'react';
 import { api } from 'trpc/client';
 
 import type { Read } from '@/table-views/api/read';
@@ -9,11 +9,14 @@ import { useCurrentCampaign } from './useCurrentCampaign';
 export const useCampaignInterviews = (): Read['output'] => {
   const { campaign } = useCurrentCampaign();
   const { search: _search } = useTableViewParams();
-  const search = useDeferredValue(_search);
-  return api.authenticated.agency.interviews.read.useSuspenseQuery({
-    campaign_id: campaign,
-    interview_stage: search.interview_stage ?? undefined,
-    updated_at: search.updated_at ?? undefined,
-    terms_accepted: search.terms_accepted ?? undefined,
-  })[0];
+  // const search = useDeferredValue(_search);
+  return api.authenticated.agency.interviews.read.useSuspenseQuery(
+    {
+      campaign_id: campaign,
+      // interview_stage: search.interview_stage ?? undefined,
+      // updated_at: search.updated_at ?? undefined,
+      // terms_accepted: search.terms_accepted ?? undefined,
+    },
+    { refetchOnMount: false },
+  )[0];
 };
