df18ad1d818560cacabfa775fe19c4eab957b3d7
Raimon T Simon <raimonrts@gmail.com>
2024-11-05 18:18:35 +0530

    404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,15 +2,13 @@ import Link from 'next/link';
 
 import Footer from '@/components/footer';
 import NursanaLogo from '@/components/nursana-logo';
-import Section from '@/components/section';
 import { Button } from '@/components/ui/button';
 
 export default function NotFound(): JSX.Element {
   return (
-    <Section>
-      <div className='h-[100vh] flex flex-col items-center justify-between pt-10'>
-      <NursanaLogo />
-      <div className='flex flex-col items-center justify-center px-4 text-center'>
+      <div className='h-[100vh] flex flex-col items-center md:justify-between pt-10'>
+        <div className='max-md:w-full max-md:px-5'> <NursanaLogo/></div>
+      <div className='flex flex-col md:items-center md:justify-center px-4 md:text-center max-md:h-full max-md:pt-16'>
         <div className='mb-4 text-6xl font-medium'>4<span className='text-purple-500'>0</span>4</div>
         <h1 className='mb-2 text-xl font-medium text-gray-800'>
           Page Not Found
@@ -24,6 +22,5 @@ export default function NotFound(): JSX.Element {
       </div>
       <Footer />
       </div>
-    </Section>
   );
 }
